0683ef453800a00b72b90487ac0f7cd4a848dc5c
Daniel Meyer <scrimshawlife@gmail.com>
2026-09-13 16:43:00 -0700

    runtime: export Hyperlex bundle helpers

--- src/noesis/hyperlex/__init__.py
+++ src/noesis/hyperlex/__init__.py
@@ -1,4 +1,11 @@
 from noesis.hyperlex.adapters import CallableHyperlexAdapter, DeterministicHyperlexAdapter, HttpHyperlexAdapter
+from noesis.hyperlex.bundle import (
+    CANONICAL_TRANSFORM_CLASSES,
+    CONTROL_TRANSFORM_CLASSES,
+    build_transform_request,
+    bundle_manifest,
+    generate_transform_bundle,
+)
 from noesis.hyperlex.materialize import MaterializedFixture, materialize_transform_result
 
 __all__ = [
@@ -6,5 +13,10 @@ __all__ = [
     "DeterministicHyperlexAdapter",
     "HttpHyperlexAdapter",
     "MaterializedFixture",
+    "CANONICAL_TRANSFORM_CLASSES",
+    "CONTROL_TRANSFORM_CLASSES",
+    "build_transform_request",
+    "bundle_manifest",
+    "generate_transform_bundle",
     "materialize_transform_result",
 ]
